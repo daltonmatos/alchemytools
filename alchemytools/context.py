62a73e56e99b6f7adb356799435d12f983bbf0d4
Dalton Barreto <daltonmatos@gmail.com>
2012-07-28 12:39:14 -0300

    Tests for commit() and close()

--- alchemytools/context.py
+++ alchemytools/context.py
@@ -5,4 +5,7 @@ from contextlib import contextmanager
 def commit_on_success(sessionClass, auto_flush=False):
     session = sessionClass()
     session.autoflush = auto_flush
+    session.autocommit = False
     yield session
+    session.commit()
+    session.close()
--- tests/context_test.py
+++ tests/context_test.py
@@ -4,6 +4,15 @@ import mock
 from alchemytools.context import commit_on_success
 
 
+class BaseDumbSession(object):
+
+    def commit(self):
+        pass
+
+    def close(self):
+        pass
+
+
 class CommitOnSuccessTest(unittest.TestCase):
 
     def setUp(self):
@@ -14,15 +23,39 @@ class CommitOnSuccessTest(unittest.TestCase):
             assert self.mock_session.call_count == 1
 
     def test_set_autoflush_default_value(self):
-        class MySession(object):
+        class MySession(BaseDumbSession):
             def __init__(self):
                 self.autoflush = True
+
         with commit_on_success(MySession) as s:
             assert s.autoflush == False
 
     def test_set_autoflush_value(self):
-        class MySession(object):
+        class MySession(BaseDumbSession):
             def __init__(self):
                 self.autoflush = True
+
         with commit_on_success(MySession, auto_flush=True) as s:
             assert s.autoflush == True
+
+    def test_set_autocommit_false(self):
+        class MySession(BaseDumbSession):
+            def __init__(self):
+                self.autocommit = True
+
+        with commit_on_success(MySession) as s:
+            assert s.autocommit == False
+
+    def test_commit_after_yield(self):
+        real_session = mock.Mock()
+        self.mock_session.return_value = real_session
+        with commit_on_success(self.mock_session):
+            pass
+        assert 1 == real_session.commit.call_count
+
+    def test_close_after_yield(self):
+        real_session = mock.Mock()
+        self.mock_session.return_value = real_session
+        with commit_on_success(self.mock_session):
+            pass
+        assert 1 == real_session.close.call_count
