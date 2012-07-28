7fc44bdd91fb1afe196406788f0e38818c7b9651
Dalton Barreto <daltonmatos@gmail.com>
2012-07-28 11:51:00 -0300

    Tests for autoflush option

--- alchemytools/context.py
+++ alchemytools/context.py
@@ -2,5 +2,7 @@ from contextlib import contextmanager
 
 
 @contextmanager
-def commit_on_success(sessionClass):
-    yield sessionClass()
+def commit_on_success(sessionClass, auto_flush=False):
+    session = sessionClass()
+    session.autoflush = auto_flush
+    yield session
--- tests/context_test.py
+++ tests/context_test.py
@@ -6,7 +6,23 @@ from alchemytools.context import commit_on_success
 
 class CommitOnSuccessTest(unittest.TestCase):
 
+    def setUp(self):
+        self.mock_session = mock.Mock()
+
     def test_call_session_class(self):
-        MySession = mock.Mock()
-        with commit_on_success(MySession):
-            assert MySession.call_count == 1
+        with commit_on_success(self.mock_session):
+            assert self.mock_session.call_count == 1
+
+    def test_set_autoflush_default_value(self):
+        class MySession(object):
+            def __init__(self):
+                self.autoflush = True
+        with commit_on_success(MySession) as s:
+            assert s.autoflush == False
+
+    def test_set_autoflush_value(self):
+        class MySession(object):
+            def __init__(self):
+                self.autoflush = True
+        with commit_on_success(MySession, auto_flush=True) as s:
+            assert s.autoflush == True
