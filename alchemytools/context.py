9f4d2535b37abfdfac8872ecb81ff5fb717f9fce
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2013-12-03 16:39:11 -0200

    Make context.managed function as decorator as well.

--- alchemytools/context.py
+++ alchemytools/context.py
@@ -10,21 +10,43 @@ class managed(object):
         self.callback = callback
         self.commit_on_success = commit_on_success
 
-    def __enter__(self):
+    def _spawn_session(self):
         sess = self.session_class()
         sess.autoflush = self.auto_flush
         sess.autocommit = self.auto_commit
         self.session = sess
         return sess
 
+    def _success(self):
+        if self.commit_on_success:
+            self.session.commit()
+
+    def _fail(self, exc_val):
+        self.session.rollback()
+        if isinstance(self.callback, Callback):
+            self.callback()
+
+    def __enter__(self):
+        return self._spawn_session()
+
     def __exit__(self, exc_type, exc_val, exc_tb):
-        if exc_type is None:
-            if self.commit_on_success:
-                self.session.commit()
+        if exc_val is None:
+            self._success()
         else:
-            self.session.rollback()
-            if isinstance(self.callback, Callback):
-                self.callback()
+            self._fail(exc_val)
         self.session.close()
         return False
+
+    def __call__(self, f):
+        def wrapped(*args, **kwargs):
+            try:
+                f(self._spawn_session(), *args, **kwargs)
+            except Exception as e:
+                self._fail(e)
+                raise e
+            else:
+                self._success()
+            finally:
+                self.session.close()
+        return wrapped
 
--- tests/context_test.py
+++ tests/context_test.py
@@ -17,7 +17,7 @@ class BaseDumbSession(object):
         pass
 
 
-class ManagedTest(unittest.TestCase):
+class ManagedAsContextManagerTest(unittest.TestCase):
 
     def setUp(self):
         self.mock_session = mock.Mock()
@@ -119,6 +119,123 @@ class ManagedTest(unittest.TestCase):
                 return False
 
         func()
+
+        assert 1 == real_session.close.call_count
+
+
+class ManagedAsDecorator(unittest.TestCase):
+
+    def setUp(self):
+        self.mock_session = mock.Mock()
+
+        def f(session):
+            pass
+
+        self.f = f
+
+    def test_call_session_class(self):
+        f = managed(self.mock_session)(self.f)
+        f()
+        assert self.mock_session.call_count == 1
+
+    def test_set_autoflush_default_value(self):
+        class MySession(BaseDumbSession):
+            def __init__(self):
+                self.autoflush = True
+
+        @managed(MySession)
+        def f(s):
+            assert s.autoflush == False
+
+    def test_set_autoflush_value(self):
+        class MySession(BaseDumbSession):
+            def __init__(self):
+                self.autoflush = False
+
+        @managed(MySession, auto_flush=True)
+        def f(s):
+            assert s.autoflush == True
+
+    def test_set_autocommit_false(self):
+        class MySession(BaseDumbSession):
+            def __init__(self):
+                self.autocommit = True
+
+        @managed(MySession)
+        def f(s):
+            assert s.autocommit == False
+
+    def test_set_autocommit_value(self):
+        class MySession(BaseDumbSession):
+            def __init__(self):
+                self.autocommit = False
+
+        @managed(MySession, auto_commit=True)
+        def f(s):
+            assert s.autocommit == True
+
+    def test_commit_if_success_when_commit_on_success_is_default(self):
+        real_session = mock.Mock()
+        self.mock_session.return_value = real_session
+
+        f = managed(self.mock_session)(self.f)
+        f()
+        assert 1 == real_session.commit.call_count
+
+    def test_dont_commit_if_success_when_commit_on_success_is_false(self):
+        real_session = mock.Mock()
+        self.mock_session.return_value = real_session
+        f = managed(self.mock_session, commit_on_success=False)(self.f)
+        f()
+        assert 0 == real_session.commit.call_count
+
+    def test_close_when_success(self):
+        real_session = mock.Mock()
+        self.mock_session.return_value = real_session
+        f = managed(self.mock_session)(self.f)
+        f()
+        assert 1 == real_session.close.call_count
+
+    def test_rollback_on_exception(self):
+        real_session = mock.Mock()
+        self.mock_session.return_value = real_session
+        raised = False
+
+        @managed(self.mock_session)
+        def f(s):
+            raise Exception()
+
+        try:
+            f()
+        except Exception as e:
+            raised = True
+
+        assert raised
+        assert 1 == real_session.rollback.call_count
+        assert 0 == real_session.commit.call_count
+        assert 1 == real_session.close.call_count
+
+    def test_callback_is_called_when_failed(self):
+        func = mock.Mock()
+        callback = Callback(func, 42, f="bar")
+
+        @managed(self.mock_session, callback=callback)
+        def f(s):
+            raise Exception()
+
+        try:
+            f()
+        except Exception as e:
+            pass
+
+        assert [mock.call(42, f='bar')] == func.call_args_list
+
+    def test_session_is_closed_on_return(self):
+        real_session = mock.Mock()
+        self.mock_session.return_value = real_session
+
+        f = managed(self.mock_session)(self.f)
+        f()
 
         assert 1 == real_session.close.call_count
 
