9f24542c479918fb3bc5d200a885ccee37435acf
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2013-12-03 15:34:35 -0200

    Transform managed in a class based context manager.

--- alchemytools/context.py
+++ alchemytools/context.py
@@ -2,20 +2,29 @@ from contextlib import contextmanager
 from callback import Callback
 
 
-@contextmanager
-def managed(sessionClass, auto_flush=False, auto_commit=False, callback=None, commit_on_success=True):
-    session = sessionClass()
-    session.autoflush = auto_flush
-    session.autocommit = auto_commit
-    try:
-        yield session
-        if commit_on_success:
-            session.commit()
-    except:
-        session.rollback()
-        if isinstance(callback, Callback):
-            callback()
-        raise
-    finally:
-        session.close()
+class managed(object):
+    def __init__(self, sessionClass, auto_flush=False, auto_commit=False, callback=None, commit_on_success=True):
+        self.session_class = sessionClass
+        self.auto_flush = auto_flush
+        self.auto_commit = auto_commit
+        self.callback = callback
+        self.commit_on_success = commit_on_success
+
+    def __enter__(self):
+        sess = self.session_class()
+        sess.autoflush = self.auto_flush
+        sess.autocommit = self.auto_commit
+        self.session = sess
+        return sess
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is None:
+            if self.commit_on_success:
+                self.session.commit()
+        else:
+            self.session.rollback()
+            if isinstance(self.callback, Callback):
+                self.callback()
+        self.session.close()
+        return False
 
